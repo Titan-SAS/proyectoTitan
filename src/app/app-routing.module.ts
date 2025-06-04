76927aef4865cf196131947e4f465a2a1695a5c9
andres-cardenas-agency <andrescardenas1930@gmail.com>
2025-06-03 20:18:29 -0500

    Primer Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './components/pages/error-page/error-page.component';
+import { HomeDemoOneComponent } from './components/pages/home-demo-one/home-demo-one.component';
+import { AboutPageComponent } from './components/pages/about-page/about-page.component';
+import { ServicesPageComponent } from './components/pages/services-page/services-page.component';
+import { ServiceDetailsPageComponent } from './components/pages/service-details-page/service-details-page.component';
+import { TeamPageComponent } from './components/pages/team-page/team-page.component';
+import { PricingPageComponent } from './components/pages/pricing-page/pricing-page.component';
+import { InvestmentFundsPageComponent } from './components/pages/investment-funds-page/investment-funds-page.component';
+import { SavingsInvestmentsPageComponent } from './components/pages/savings-investments-page/savings-investments-page.component';
+import { PrivacyPolicyPageComponent } from './components/pages/privacy-policy-page/privacy-policy-page.component';
+import { TermsConditionsPageComponent } from './components/pages/terms-conditions-page/terms-conditions-page.component';
+
+const routes: Routes = [
+    {path: '', component: HomeDemoOneComponent},
+    {path: 'about', component: AboutPageComponent},
+    {path: 'services', component: ServicesPageComponent},
+    {path: 'service-details', component: ServiceDetailsPageComponent},
+    {path: 'team', component: TeamPageComponent},
+    {path: 'pricing', component: PricingPageComponent},
+    {path: 'investment-funds', component: InvestmentFundsPageComponent},
+    {path: 'savings-investments', component: SavingsInvestmentsPageComponent},
+    {path: 'privacy-policy', component: PrivacyPolicyPageComponent},
+    {path: 'terms-conditions', component: TermsConditionsPageComponent},
+    // Here add new pages component
+
+    {path: '**', component: ErrorPageComponent} // This line will remain down from the whole pages component list
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
